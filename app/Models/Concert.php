8feea5cd5cb1207af37599643c84dd642f7e1d38
Anastasia <anastasiakosovets@gmail.com>
2023-07-12 23:07:11 +0200

    feat: modified Concert model

---
+++ app/Models/Concert.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Concert extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'group_id',
+        'title',
+        'date',
+        'groupName',
+        'description',
+        'programm'
+    ];
+    protected $casts = [
+        'date' => 'datetime'
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function group(){
+        return $this->belongsTo(Group::class, 'group_id');
+    }
+}
